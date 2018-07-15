a82f4dbcd68f7eff8505eef7ebec9994e81f26bc
Safnaj <37530024+Safnaj@users.noreply.github.com>
2018-07-16 00:20:45 +0530

    Project 2
    updated

---
+++ src/sms/db/DBConnection.java
@@ -0,0 +1,23 @@
+package sms.db;
+
+import java.sql.*;
+public class DBConnection{
+    private Connection conn;
+    private static DBConnection dbConnection;
+
+    private DBConnection()throws ClassNotFoundException,SQLException{
+        Class.forName("com.mysql.jdbc.Driver");
+        conn=DriverManager.getConnection("jdbc:mysql://localhost/sms","root","");
+    }
+    public Connection getConnection(){
+        return conn;
+    }
+    public static DBConnection getDBConnection()throws ClassNotFoundException,SQLException{
+        if(dbConnection==null){
+            dbConnection=new DBConnection();
+        }
+        return dbConnection;
+    }
+
+}
+
